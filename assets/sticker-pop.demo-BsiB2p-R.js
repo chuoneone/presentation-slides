b92511aad6261e2f2f1bbf58012f2294ff813055
chuoneone <90313941+chuoneone@users.noreply.github.com>
2026-08-27 11:17:20 +0000

    Deploying to gh-pages from @ chuoneone/presentation-slides@1270f71e516ec262afa73e69c8d7bafc28ca7a30 🚀

--- assets/sticker-pop.demo-BsiB2p-R.js
+++ assets/sticker-pop.demo-BsiB2p-R.js
@@ -1,4 +1,4 @@
-import{j as e}from"./index-DGzsPYT3.js";import{u as g}from"./index-Bff2ea4e.js";const a=`
+import{j as e}from"./index-CM2ZD3Vs.js";import{u as g}from"./index-BFzD-6M3.js";const a=`
 @keyframes sp-pop {
   0%   { transform: scale(0.92) rotate(var(--sp-tilt, 0deg)); opacity: 0; }
   60%  { transform: scale(1.04) rotate(var(--sp-tilt, 0deg)); opacity: 1; }
