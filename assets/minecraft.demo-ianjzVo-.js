1e6f2a153a2a6c12ff97c3026c40298d787735f3
chuoneone <90313941+chuoneone@users.noreply.github.com>
2026-08-27 09:05:29 +0000

    Deploying to gh-pages from @ chuoneone/presentation-slides@80206a8153e27a7619dac29c97f5f099b72825c3 🚀

--- assets/minecraft.demo-ianjzVo-.js
+++ assets/minecraft.demo-ianjzVo-.js
@@ -1,4 +1,4 @@
-import{j as e}from"./index-DPCP8nUS.js";import{u as N}from"./index-fzNqMqJp.js";const D=`
+import{j as e}from"./index-Dm8VfqFL.js";import{u as N}from"./index-DVw-IeQK.js";const D=`
 @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&family=VT323&display=swap');
 
 @keyframes mc-pop {
--- assets/sticker-pop.demo-Lq4_xGNW.js
+++ assets/sticker-pop.demo-Lq4_xGNW.js
@@ -1,4 +1,4 @@
-import{j as e}from"./index-DPCP8nUS.js";import{u as g}from"./index-fzNqMqJp.js";const a=`
+import{j as e}from"./index-Dm8VfqFL.js";import{u as g}from"./index-DVw-IeQK.js";const a=`
 @keyframes sp-pop {
   0%   { transform: scale(0.92) rotate(var(--sp-tilt, 0deg)); opacity: 0; }
   60%  { transform: scale(1.04) rotate(var(--sp-tilt, 0deg)); opacity: 1; }
